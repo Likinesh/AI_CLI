6f71e0d005e28b2b703edc39e3a3443e570207cb
Likinesh <likithkk2004@gmail.com>
2025-12-26 19:59:52 +0530

    Authentication

---
+++ client/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,12 @@
+import { LoginForm } from '@/components/login-form'
+import React from 'react'
+
+const SignIn = () => {
+  return (
+    <>
+        <LoginForm />
+    </>
+  )
+}
+
+export default SignIn
--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,29 @@
 import express from "express";
 import "dotenv/config";
+import { fromNodeHeaders, toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth.js";
+import cors from "cors"
 
 const app = express();
+
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+        methods: ["GET","POST","PUT","DELETE"],
+        credentials: true,
+    })
+)
+
+app.all('/api/auth/{*any}', toNodeHandler(auth));
+
+app.use(express.json());
+
+app.get("/api/me", async(req,res)=>{
+    const session = await auth.api.getSession({
+        headers: fromNodeHeaders(req.headers),
+    });
+    return res.json(session);
+})
 
 app.get("/health", (req,res)=>{
     res.send("OK");
